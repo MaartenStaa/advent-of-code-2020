393361f41497786774336ef35c791dc8a01e6ef7
Maarten Staa <maarten.staa@brightalley.nl>
2020-12-03 20:46:05 +0100

    Day 1, part 1.

---
+++ day1-1/src/main.rs
@@ -0,0 +1,22 @@
+use std::num::ParseIntError;
+
+const INPUT: &'static str = include_str!("../input.txt");
+const EXPECTED: u32 = 2020;
+
+fn main() -> Result<(), ParseIntError> {
+    let lines: Vec<_> = INPUT.lines().collect();
+    'outer: for (index, a) in lines.iter().enumerate() {
+        let a: u32 = a.parse()?;
+        for b in lines.iter().skip(index + 1) {
+            let b: u32 = b.parse()?;
+            if a + b == EXPECTED {
+                println!("{} + {} = {}", a, b, EXPECTED);
+                println!("{} * {} = {}", a, b, a * b);
+
+                break 'outer;
+            }
+        }
+    }
+
+    Ok(())
+}
